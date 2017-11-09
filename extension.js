30aeaff864c544d67e42ff7f0afcfb6aaf6c7771
Matthew Hummer <biggreyson@gmail.com>
2017-11-08 20:59:19 -0700

    change some styling of the denial

--- extension.js
+++ extension.js
@@ -1,7 +1,6 @@
 let host = window.location.hostname;
 let body = document.getElementsByTagName('body')[0];
 let head = document.getElementsByTagName('head')[0];
-console.log(head);
 if (true){
   while (head.children.length >0){
     head.removeChild(head.children[0]);
@@ -9,15 +8,35 @@ if (true){
   while (body.children.length > 0){
     body.removeChild(body.children[0]);
   }
-  let styles = document.createElement('link');
-  styles.rel = "stylesheet";
-  styles.type = "text/css";
-  styles.href = "modal.css";
-  head.append(styles);
+  let styles = document.createElement('style');
+  styles.innerHTML = `
+    html {
+      background-color: #202328;
+    }
+    h1 {
+      text-align: center;
+    }
+    p {
+      text-align: center;
+    }
+    #modal {
+      background-color: white;
+      border-radius: 25px;
+    }
+  `
+  body.append(styles);
+
+
   let outerDiv = document.createElement('div');
   outerDiv.id = "modal";
+
   let newh1 = document.createElement('h1');
-  newh1.innerHTML = 'get back to work, you bum!';
+  newh1.innerHTML = 'Get out of here, you!';
   outerDiv.append(newh1);
+
+  let warning = document.createElement('p');
+  warning.innerHTML = 'It looks like this is one of those sites that you have decided not to use any longer.';
+  outerDiv.append(warning);
+
   body.append(outerDiv);
 }
