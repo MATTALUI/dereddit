018a83ea8c5e6ad14585f1dd7084679e559abb4f
Matthew Hummer <biggreyson@gmail.com>
2017-11-09 20:14:58 -0700

    xhr for forbidden sites

--- extension.js
+++ extension.js
@@ -1,18 +1,22 @@
-// httpRequest = new XMLHttpRequest();
-// httpRequest.onreadystatechange = handleRequest;
-// httpRequest.open('GET', 'example.com', true);
-// httpRequest.send();
-handleRequest();
+httpRequest = new XMLHttpRequest();
+httpRequest.onreadystatechange = handleRequest;
+httpRequest.open('GET', 'https://myblocker.herokuapp.com/temp', true);
+httpRequest.send();
+// handleRequest();
 
 function handleRequest(){
-  // if (httpRequest.readyState === 4){
-    // const data = JSON.parse(httpRequest.responseText);
-    // console.log(data);
-
+  if (httpRequest.readyState === 4){
+    const data = JSON.parse(httpRequest.responseText);
     let host = window.location.hostname;
     let body = document.getElementsByTagName('body')[0];
     let head = document.getElementsByTagName('head')[0];
-    if (true){
+    let blocked = false;
+    data.blocked.forEach((blockedSite)=>{
+      if (blockedSite.indexOf(host) > -1) {
+        blocked = true;
+      }
+    });
+    if (blocked){
       while (head.children.length >0){
         head.removeChild(head.children[0]);
       }
@@ -62,7 +66,7 @@ function handleRequest(){
       body.append(outerDiv);
       // document.getElementById('moreButton').addEventListener('click', clickMoreButton);
     }
-  // }
+  }
 }
 function clickMoreButton(){
   console.log('show more!');
